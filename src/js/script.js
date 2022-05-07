7827d8de9f81e1133f5e09edb864a5a1e4f0a657
Maxim <karpov159@mail.ru>
2022-05-07 13:35:03 +0300

    created next page

--- src/js/script.js
+++ src/js/script.js
@@ -6,9 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
           apiSearch = 'https://api.themoviedb.org/3/search/movie/?api_key=04c35731a5ee918f014970082a0088b1&query=',
           apiTopMovies = 'https://api.themoviedb.org/3/movie/top_rated?api_key=04c35731a5ee918f014970082a0088b1&page=1',
           apiTopTV = 'https://api.themoviedb.org/3/tv/popular?api_key=04c35731a5ee918f014970082a0088b1&page=1',
-          apiAnim = 'https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1&with_genres=16&sort_by=vote_count.desc&vote_count.gte=10',
+          apiAnim = 'https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1&with_genres=16&sort_by=vote_count.desc&vote_count.gte=10&page=1',
           apiActors = 'https://api.themoviedb.org/3/person/popular?api_key=04c35731a5ee918f014970082a0088b1&page=1',
           imgPath = "https://image.tmdb.org/t/p/w1280",
+          nextArrow = document.getElementById('next'),
           form = document.querySelector('form'),
           linkMovies = document.getElementById('movies'),
           linkTvShows = document.getElementById('tvshows'),
@@ -20,8 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
           inputSearch = document.querySelector('.header__option-input'),
           parentMovies = document.querySelector('.main__movies-wrapper');
 
-
-
+    const page = {
+        api: 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=',
+        num: 1,
+        name: 'Popular movies'
+    };
     const colors = ['rgb(243, 121, 76)', 'rgb(87, 218, 241)', 'rgb(204, 130, 238)', 'rgb(98, 206, 179)'];
     
 
@@ -140,6 +144,10 @@ document.addEventListener('DOMContentLoaded', () => {
         const name = category.innerHTML;
                     
         mainTitle.innerHTML = name;
+        page.name = name;
+        page.num = 1;
+        page.api = api.slice(0, api.length-1);
+        // console.log(api);
 
         category.classList.add('header__link-active');
 
@@ -156,7 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
             this.rating = rating;
             this.overview = overview;
             this.poster = poster;
-            // this.data = data.slice(0,4);
+            this.data = data;
             this.parent = document.querySelector(parent);
         }
 
@@ -228,7 +236,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"/></svg>
                     </a>
                         <div class="main__overview-rate">${this.rating}</div>
-                        <div class="main__overview-date">${this.data}</div>
+                        <div class="main__overview-date">${this.data.slice(0,4)}</div>
                     </div>
                     <div class="main__overview-descr">${this.overview}</div>
                     <div class="main__overview-btns">
@@ -365,10 +373,15 @@ document.addEventListener('DOMContentLoaded', () => {
                     const name = genre.innerHTML;
 
                     mainTitle.innerHTML = name;
+                    page.name = name;
+                    page.num = 1;
+                    console.log(page);
 
                     const num = await getNumOfGenre(name);
 
-                    const url = `https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1&with_genres=${num}&sort_by=vote_count.desc&vote_count.gte=10`;
+                    const url = `https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1&with_genres=${num}&sort_by=vote_count.desc&vote_count.gte=10&page=1`;
+
+                    page.api = url;
                     
                     deleteMovies();
 
@@ -401,12 +414,34 @@ document.addEventListener('DOMContentLoaded', () => {
         const name = linkActors.innerHTML;
                     
         mainTitle.innerHTML = name;
+        page.api = apiActors.substring(0, apiActors.length-1);
+        page.name = name;
+        page.num = 1;
 
         linkActors.classList.add('header__link-active');
 
         showActors(apiActors);
     });
 
+    //обработчик на кнопку next
+
+    nextArrow.addEventListener('click', () => {
+        if(page.name == 'Actors') {
+            deleteActors();
+            console.log(page)
+
+            page.num++;
+            showActors(page.api + page.num);
+        } else {
+            // showActors(page.api);
+            console.log(page)
+            page.num++;
+            deleteMovies();
+            showMovies(page.api + page.num);
+        }
+    });
+
+    
     // показ популярных фильмов
     showMovies(apiPopularMovies);
     colorGenres();
@@ -414,3 +449,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 });
+
